94da4fd3d2f8a443fda0679165ed8d24a08bfce0
Rabia790 <rabiaangel9921@gmail.com>
2022-11-07 18:47:11 -0500

    added random number

--- Rabia_c0880823_FinalExam/ViewController.swift
+++ Rabia_c0880823_FinalExam/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var randomNum: UILabel!
+    var timer: Timer?
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.bnbbb
+        timer =  Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true) { (timer) in
+            
+            self.setRandomvalue()
+            
+            
+        }
+       
     }
-
-
+    private func setRandomvalue(){
+        
+        let randomInt = Int.random(in: 1..<100)
+        randomNum.text = "\(randomInt)"
+        //RandomNumber.isHidden = false
+    }
+   
 }
 
